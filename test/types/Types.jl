d5c1384726c9e8ed9d501dbcfbe424fee72cecf1
Pablo Valdunciel <pablo.valdunciel@docyet.com>
2025-03-21 14:38:21 +0100

    format files using JuliaFormatter.jl

--- test/types/Types.jl
+++ test/types/Types.jl
@@ -1,23 +1,23 @@
-using RAGTools: ChunkEmbeddingsIndex, 
-                ChunkKeywordsIndex,
-                MultiIndex,
-                CandidateChunks,
-                MultiCandidateChunks,
-                AbstractCandidateChunks, 
-                DocumentTermMatrix,
-                SubDocumentTermMatrix,
-                document_term_matrix, 
-                HasEmbeddings,
-                HasKeywords,
-                ChunkKeywordsIndex, 
-                AbstractChunkIndex,
-                AbstractDocumentIndex
+using RAGTools: ChunkEmbeddingsIndex,
+	ChunkKeywordsIndex,
+	MultiIndex,
+	CandidateChunks,
+	MultiCandidateChunks,
+	AbstractCandidateChunks,
+	DocumentTermMatrix,
+	SubDocumentTermMatrix,
+	document_term_matrix,
+	HasEmbeddings,
+	HasKeywords,
+	ChunkKeywordsIndex,
+	AbstractChunkIndex,
+	AbstractDocumentIndex
 using RAGTools: embeddings, chunks, tags, tags_vocab, sources,
-                extras, positions, scores, parent,
-                RAGResult, chunkdata, preprocess_tokens, tf,
-                vocab, vocab_lookup, idf, doc_rel_length
+	extras, positions, scores, parent,
+	RAGResult, chunkdata, preprocess_tokens, tf,
+	vocab, vocab_lookup, idf, doc_rel_length
 using RAGTools: SubChunkIndex, indexid, indexids,
-                translate_positions_to_parent
+	translate_positions_to_parent
 using PromptingTools: last_message, last_output
 
 
